0908f601e48740b10fa261e1e349221f637287e8
hadarbmdev <hadarbmdev@gmail.com>
2019-02-12 15:06:40 +0200

    push last changes

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	console.log("users request")
   res.json([{
   	id: 1,
   	username: "samsepi0l"
